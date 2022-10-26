5984c1d85cf913e34b1269d2b9cde0ccaa909fb5
Shwe Pyi Hein <shwepyihein.12@gmail.com>
2022-10-26 14:02:44 +0700

    add google ads scenc

--- mm-course/pages/_app.tsx
+++ mm-course/pages/_app.tsx
@@ -7,6 +7,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Hunter dox</title>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6467325004781249"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <Component {...pageProps} />
     </>
