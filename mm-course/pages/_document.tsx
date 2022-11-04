6b30725e39cbcbba7044d89c2c3bf8bd100002f1
Shwe Pyi Hein <shwepyihein.12@gmail.com>
2022-11-04 16:53:02 +0700

    change document

--- mm-course/pages/_document.tsx
+++ mm-course/pages/_document.tsx
@@ -1,15 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document"
 
 class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
   render() {
     return (
       <Html>
-        <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/logo_3.svg" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/logo_3.svg" />
-          <meta name="msapplication-TileColor" content="#f2900A" />
-          <meta name="theme-color" content="#ffffff"></meta>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
