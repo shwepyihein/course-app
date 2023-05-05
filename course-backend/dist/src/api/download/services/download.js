b7961e086a95277af3b272e1e1e80bdc72cd53e0
Ko Nyan <devkonyan@gmail.com>
2023-05-05 13:45:20 +0700

    fix database

---
+++ course-backend/dist/src/api/download/services/download.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * download service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::download.download');
